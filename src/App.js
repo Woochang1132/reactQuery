2eb131248ed6e3fc89784a9fee041b0f81bbe964
Woochang1132 <show4031132@gmail.com>
2023-10-25 16:46:32 +0900

    최우창 react-query, useQuery, ReactQueryDevtools 활용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
 import "./App.css";
 import MainProducts from "./components/MainProducts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-function App() {
-  return <MainProducts />;
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainProducts />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
 }
-
-export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
-import useProducts from '../hooks/useProducts';
+import {useQuery} from '@tanstack/react-query';
 
 export default function Products() {
   const [checked, setChecked] = useState(false);
-  const [loading, error, products] = useProducts({ salesOnly: checked });
+  const {isLoading, error, data:products} = useQuery({
+    queryKey:['products', checked],
+    queryFn:async () => {
+      console.log("fetching...");
+      return fetch(`data/${checked ? 'sale_' : ''}products.json`).then((res) => res.json());
+    },
+  })
+
   const handleChange = () => setChecked((prev) => !prev);
 
-  if (loading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error}</p>;
 
