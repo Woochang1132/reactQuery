64cf2c945ebc1af8abcbdde59051fafee91a52d7
Woochang1132 <show4031132@gmail.com>
2023-10-25 13:21:32 +0900

    최우창 react_query, re-render 오류 해결

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,27 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export default function useProducts({salesOnly}) {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState();
-    const [products, setProducts] = useState([]);
+export default function useProducts({ salesOnly }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    console.log('fetching....');
+    setLoading(true);
+    setError(undefined);
+    fetch(`data/${salesOnly ? 'sale_' : ''}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
+        setProducts(data);
+      })
+      .catch((e) => setError('에러가 발생했음!'))
+      .finally(() => setLoading(false));
+    return () => {
+      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
+    };
+  }, [salesOnly]);
 
-    useEffect(() => {
-        console.log("fetching...");
-        setLoading(true);
-        setError(undefined);
-        fetch(`data/${salesOnly ? 'sale_' : ''}products.json`)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log("데이터를 네트워크에서 받아옴")
-                setProducts(data);
-            })
-            .catch((e) => setError('에러가 발생했음'))
-            .finally(() => setLoading(false));
-            return () => {
-                console.log('data 청소를 진행합니다.')
-            }
-    }, [salesOnly])
-
-    return [loading, error, products];
+  return [loading, error, products];
 }
-
